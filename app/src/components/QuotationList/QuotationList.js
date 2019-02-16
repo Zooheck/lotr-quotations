8cb44dc53145b48991a1326e6d78dd1fd97d55ab
Zooheck <davidscottflack@gmail.com>
2019-02-15 23:34:15 -0600

    Failed pagination attempt

--- app/src/components/QuotationList/QuotationList.js
+++ app/src/components/QuotationList/QuotationList.js
@@ -7,20 +7,27 @@ import { getPosts } from '../../actions/index.js'
 import Quotation from '../Quotation/Quotation.js'
 
 class QuotationList extends Component {
+  state = {
+    currentPost: 0
+  }
   componentDidMount() {
     this.props.getPosts(this.props.character.id)
   }
-  render() {
-    return (
-      <div>
-        {this.props.posts.map(post => {
-          return(
-            <Quotation post={post} />
-          )
-        })}
-      </div>
-    )
+  nextQuotation = e => {
+    e.preventDefault()
+    console.log(this.props.posts[0].text)
   }
+  render() {
+      return (
+        <div>
+          {this.props.posts.map(post => {
+            return(
+              <Quotation post={post} />
+            )
+          })}
+        </div>
+      )
+    }
 }
 
 const mapStateToProps = state => ({
